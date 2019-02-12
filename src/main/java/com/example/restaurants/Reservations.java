1a11e673cda35f9ac72c8ad36bf131f00ec512e6
tariqzayn <abdurrazaky@gmail.com>
2019-02-12 13:41:58 +0100

    first commit

---
+++ src/main/java/com/example/restaurants/Reservations.java
@@ -0,0 +1,49 @@
+package com.example.restaurants;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reservations")
+public class Reservations {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String timeFrom;
+
+    @Column(nullable = false)
+    private String timeTo;
+
+    @ManyToOne
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public Reservations(String timeFrom, String timeTo) {
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
+    }
+
+    public String getTimeFrom() {
+        return timeFrom;
+    }
+
+    public void setTimeFrom(String timeFrom) {
+        this.timeFrom = timeFrom;
+    }
+
+    public String getTimeTo() {
+        return timeTo;
+    }
+
+    public void setTimeTo(String timeTo) {
+        this.timeTo = timeTo;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
---
+++ src/main/java/com/example/restaurants/User.java
@@ -0,0 +1,125 @@
+package com.example.restaurants;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    private boolean admin;
+
+    public User(String username, String password, String name, String lastName, String phoneNumber, String email, boolean admin, String country, String city) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.admin = admin;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
